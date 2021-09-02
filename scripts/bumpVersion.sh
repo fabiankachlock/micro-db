b2b8800541dd77a405b551223324ba9271662388
fabiankachlock <fabian.kachlock@icloud.com>
2021-09-02 23:00:37 +0200

    add bump version script

---
+++ scripts/bumpVersion.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "bump $1 for node-micro-db"
+
+if ! [ "$1" == "patch" ] || [ "$1" == "major" ] || [ "$1" == "minor" ]; then
+	echo "unknown $1"
+	exit 1
+fi
+
+if ! [ "$(git branch --show-current)" == "main" ]; then
+ exit 1
+fi
+
+oldVersion=$(npm info node-micro-db version)
+
+echo "on main..."
+echo "old version $oldVersion"
+
+echo "running checks..."
+yarn test
+yarn lint
+
+echo "formatting..."
+yarn format
+
+echo "building package..."
+yarn build
+
+echo "bumping version"
+npm version $1
+
+echo "bumped version"
+newVersion=$(npm info node-micro-db version)
+echo "new version newVersion"
+
+echo "publishing..."
+git push --tags
+npm publish --access=public
+git push
+
+echo "succesfully $newVersion"
